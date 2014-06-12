6e60bbdb65d9151d2c5e555f3882985721bc86bd
Antoine Grondin <antoinegrondin@gmail.com>
2014-06-11 20:48:01 -0400

    Benchmark keys that exist.

--- benchmark.md
+++ benchmark.md
@@ -1,31 +1,28 @@
 ```
 PASS
-Benchmark_map_SmallStr_0	500000000	         5.06 ns/op	       0 B/op	       0 allocs/op
-Benchmark_map_SmallStr_4	50000000	        46.0 ns/op	       0 B/op	       0 allocs/op
-Benchmark_map_SmallStr_8	50000000	        46.1 ns/op	       0 B/op	       0 allocs/op
-Benchmark_map_SmallStr_16	50000000	        33.2 ns/op	       0 B/op	       0 allocs/op
-Benchmark_map_SmallStr_32	50000000	        33.3 ns/op	       0 B/op	       0 allocs/op
-Benchmark_map_SmallStr_64	50000000	        35.7 ns/op	       0 B/op	       0 allocs/op
-Benchmark_map_SmallStr_512	50000000	        33.6 ns/op	       0 B/op	       0 allocs/op
-Benchmark_map_SmallStr_1024	50000000	        33.3 ns/op	       0 B/op	       0 allocs/op
-Benchmark_map_SmallStr_1M	50000000	        33.2 ns/op	       0 B/op	       0 allocs/op
-Benchmark_ternaryST_SmallStr_0	50000000	        58.7 ns/op	       8 B/op	       1 allocs/op
-Benchmark_ternaryST_SmallStr_4	20000000	        76.8 ns/op	       8 B/op	       1 allocs/op
-Benchmark_ternaryST_SmallStr_8	20000000	        95.9 ns/op	       8 B/op	       1 allocs/op
-Benchmark_ternaryST_SmallStr_16	20000000	       104 ns/op	       8 B/op	       1 allocs/op
-Benchmark_ternaryST_SmallStr_32	20000000	        94.9 ns/op	       8 B/op	       1 allocs/op
-Benchmark_ternaryST_SmallStr_64	20000000	       118 ns/op	       8 B/op	       1 allocs/op
-Benchmark_ternaryST_SmallStr_512	20000000	       122 ns/op	      16 B/op	       1 allocs/op
-Benchmark_ternaryST_SmallStr_1024	20000000	       132 ns/op	      16 B/op	       1 allocs/op
-Benchmark_ternaryST_SmallStr_1M	 5000000	       411 ns/op	      32 B/op	       1 allocs/op
-Benchmark_trie128_SmallStr_0	20000000	       100 ns/op	      17 B/op	       1 allocs/op
-Benchmark_trie128_SmallStr_4	20000000	       105 ns/op	      17 B/op	       1 allocs/op
-Benchmark_trie128_SmallStr_8	20000000	       105 ns/op	      17 B/op	       1 allocs/op
-Benchmark_trie128_SmallStr_16	20000000	       109 ns/op	      17 B/op	       1 allocs/op
-Benchmark_trie128_SmallStr_32	20000000	       108 ns/op	      17 B/op	       1 allocs/op
-Benchmark_trie128_SmallStr_64	20000000	       109 ns/op	      17 B/op	       1 allocs/op
-Benchmark_trie128_SmallStr_512	20000000	       107 ns/op	      17 B/op	       1 allocs/op
-Benchmark_trie128_SmallStr_1024	20000000	       111 ns/op	      17 B/op	       1 allocs/op
-Benchmark_trie128_SmallStr_1M	20000000	       119 ns/op	      17 B/op	       1 allocs/op
-ok  	github.com/aybabtme/trie	113.097s
+Benchmark_map_SmallStr_4	500000000	         6.83 ns/op	       0 B/op	       0 allocs/op
+Benchmark_map_SmallStr_8	500000000	         6.76 ns/op	       0 B/op	       0 allocs/op
+Benchmark_map_SmallStr_16	100000000	        22.8 ns/op	       0 B/op	       0 allocs/op
+Benchmark_map_SmallStr_32	100000000	        23.2 ns/op	       0 B/op	       0 allocs/op
+Benchmark_map_SmallStr_64	100000000	        18.9 ns/op	       0 B/op	       0 allocs/op
+Benchmark_map_SmallStr_512	100000000	        23.3 ns/op	       0 B/op	       0 allocs/op
+Benchmark_map_SmallStr_1024	50000000	        32.7 ns/op	       0 B/op	       0 allocs/op
+Benchmark_map_SmallStr_1M	50000000	        32.5 ns/op	       0 B/op	       0 allocs/op
+Benchmark_ternaryST_SmallStr_4	50000000	        66.9 ns/op	       8 B/op	       0 allocs/op
+Benchmark_ternaryST_SmallStr_8	50000000	        66.6 ns/op	       8 B/op	       0 allocs/op
+Benchmark_ternaryST_SmallStr_16	20000000	        80.2 ns/op	       8 B/op	       0 allocs/op
+Benchmark_ternaryST_SmallStr_32	20000000	        79.6 ns/op	       8 B/op	       0 allocs/op
+Benchmark_ternaryST_SmallStr_64	20000000	        80.4 ns/op	       8 B/op	       0 allocs/op
+Benchmark_ternaryST_SmallStr_512	20000000	       109 ns/op	      16 B/op	       1 allocs/op
+Benchmark_ternaryST_SmallStr_1024	20000000	       118 ns/op	      16 B/op	       1 allocs/op
+Benchmark_ternaryST_SmallStr_1M	10000000	       213 ns/op	      32 B/op	       1 allocs/op
+Benchmark_trie128_SmallStr_4	100000000	        24.4 ns/op	       0 B/op	       0 allocs/op
+Benchmark_trie128_SmallStr_8	100000000	        24.2 ns/op	       0 B/op	       0 allocs/op
+Benchmark_trie128_SmallStr_16	50000000	        31.2 ns/op	       0 B/op	       0 allocs/op
+Benchmark_trie128_SmallStr_32	50000000	        31.5 ns/op	       0 B/op	       0 allocs/op
+Benchmark_trie128_SmallStr_64	50000000	        31.6 ns/op	       0 B/op	       0 allocs/op
+Benchmark_trie128_SmallStr_512	50000000	        38.7 ns/op	       0 B/op	       0 allocs/op
+Benchmark_trie128_SmallStr_1024	50000000	        45.5 ns/op	       0 B/op	       0 allocs/op
+Benchmark_trie128_SmallStr_1M	50000000	        65.9 ns/op	       0 B/op	       0 allocs/op
+ok  	github.com/aybabtme/trie	94.029s
 ```
--- benchmark_test.go
+++ benchmark_test.go
@@ -6,57 +6,76 @@ import (
 	"testing"
 )
 
-func Benchmark_map_SmallStr_0(b *testing.B)    { benchmark_map_SmallStr(b, 0, false) }
-func Benchmark_map_SmallStr_4(b *testing.B)    { benchmark_map_SmallStr(b, 4, false) }
-func Benchmark_map_SmallStr_8(b *testing.B)    { benchmark_map_SmallStr(b, 8, false) }
-func Benchmark_map_SmallStr_16(b *testing.B)   { benchmark_map_SmallStr(b, 16, false) }
-func Benchmark_map_SmallStr_32(b *testing.B)   { benchmark_map_SmallStr(b, 32, false) }
-func Benchmark_map_SmallStr_64(b *testing.B)   { benchmark_map_SmallStr(b, 64, false) }
-func Benchmark_map_SmallStr_512(b *testing.B)  { benchmark_map_SmallStr(b, 512, false) }
-func Benchmark_map_SmallStr_1024(b *testing.B) { benchmark_map_SmallStr(b, 1024, false) }
-func Benchmark_map_SmallStr_1M(b *testing.B)   { benchmark_map_SmallStr(b, 1<<20, false) }
+func Benchmark_map_SmallStr_4(b *testing.B)    { benchmark_map_SmallStr(b, 4, 128) }
+func Benchmark_map_SmallStr_8(b *testing.B)    { benchmark_map_SmallStr(b, 8, 128) }
+func Benchmark_map_SmallStr_16(b *testing.B)   { benchmark_map_SmallStr(b, 16, 128) }
+func Benchmark_map_SmallStr_32(b *testing.B)   { benchmark_map_SmallStr(b, 32, 128) }
+func Benchmark_map_SmallStr_64(b *testing.B)   { benchmark_map_SmallStr(b, 64, 128) }
+func Benchmark_map_SmallStr_512(b *testing.B)  { benchmark_map_SmallStr(b, 512, 128) }
+func Benchmark_map_SmallStr_1024(b *testing.B) { benchmark_map_SmallStr(b, 1024, 128) }
+func Benchmark_map_SmallStr_1M(b *testing.B)   { benchmark_map_SmallStr(b, 1<<20, 128) }
 
-func benchmark_map_SmallStr(b *testing.B, keys int, two bool) {
+func benchmark_map_SmallStr(b *testing.B, keys, alphaSize int) {
 	runtime.GC()
+	b.ReportAllocs()
 	m := make(map[string]bool)
+	var longest string
 	for i := 0; i < keys; i++ {
-		m[fmt.Sprint(i)] = true
-	}
-	b.ResetTimer()
-	key := fmt.Sprint(keys + 1)
-	for i := 0; i < b.N; i++ {
-		if two {
-			_, _ = m[key]
-		} else {
-			_ = m[key]
+		str := fmt.Sprint(i)
+		m[str] = true
+		if len(longest) < len(str) {
+			longest = str
 		}
 	}
-}
-
-func Benchmark_ternaryST_SmallStr_0(b *testing.B)    { benchmark_ternaryST_SmallStr(b, 0) }
-func Benchmark_ternaryST_SmallStr_4(b *testing.B)    { benchmark_ternaryST_SmallStr(b, 4) }
-func Benchmark_ternaryST_SmallStr_8(b *testing.B)    { benchmark_ternaryST_SmallStr(b, 8) }
-func Benchmark_ternaryST_SmallStr_16(b *testing.B)   { benchmark_ternaryST_SmallStr(b, 16) }
-func Benchmark_ternaryST_SmallStr_32(b *testing.B)   { benchmark_ternaryST_SmallStr(b, 32) }
-func Benchmark_ternaryST_SmallStr_64(b *testing.B)   { benchmark_ternaryST_SmallStr(b, 64) }
-func Benchmark_ternaryST_SmallStr_512(b *testing.B)  { benchmark_ternaryST_SmallStr(b, 512) }
-func Benchmark_ternaryST_SmallStr_1024(b *testing.B) { benchmark_ternaryST_SmallStr(b, 1024) }
-func Benchmark_ternaryST_SmallStr_1M(b *testing.B)   { benchmark_ternaryST_SmallStr(b, 1<<20) }
-
-func benchmark_ternaryST_SmallStr(b *testing.B, keys int) {
-	runtime.GC()
-	trie := NewTernaryST()
-	for i := 0; i < keys; i++ {
-		trie.Put(fmt.Sprint(i), true)
-	}
+	// b.Logf("longest=%q", longest)
 	b.ResetTimer()
-	key := fmt.Sprint(keys + 1)
+
+	allTrue := true
+	var resp, ok bool
 	for i := 0; i < b.N; i++ {
-		_, _ = trie.Get(key)
+		resp, ok = m[longest]
+		allTrue = allTrue && resp && ok
+	}
+	if !allTrue {
+		b.Fatal("allTrue should always be true")
 	}
 }
 
-func Benchmark_trie128_SmallStr_0(b *testing.B)    { benchmark_trie_SmallStr(b, 0, 128) }
+func Benchmark_ternaryST_SmallStr_4(b *testing.B)    { benchmark_ternaryST_SmallStr(b, 4, 128) }
+func Benchmark_ternaryST_SmallStr_8(b *testing.B)    { benchmark_ternaryST_SmallStr(b, 8, 128) }
+func Benchmark_ternaryST_SmallStr_16(b *testing.B)   { benchmark_ternaryST_SmallStr(b, 16, 128) }
+func Benchmark_ternaryST_SmallStr_32(b *testing.B)   { benchmark_ternaryST_SmallStr(b, 32, 128) }
+func Benchmark_ternaryST_SmallStr_64(b *testing.B)   { benchmark_ternaryST_SmallStr(b, 64, 128) }
+func Benchmark_ternaryST_SmallStr_512(b *testing.B)  { benchmark_ternaryST_SmallStr(b, 512, 128) }
+func Benchmark_ternaryST_SmallStr_1024(b *testing.B) { benchmark_ternaryST_SmallStr(b, 1024, 128) }
+func Benchmark_ternaryST_SmallStr_1M(b *testing.B)   { benchmark_ternaryST_SmallStr(b, 1<<20, 128) }
+
+func benchmark_ternaryST_SmallStr(b *testing.B, keys, alphaSize int) {
+	runtime.GC()
+	b.ReportAllocs()
+	trie := NewTernaryST()
+	var longest string
+	for i := 0; i < keys; i++ {
+		str := fmt.Sprint(i)
+		trie.Put(str, true)
+		if len(longest) < len(str) {
+			longest = str
+		}
+	}
+	// b.Logf("longest=%q", longest)
+	b.ResetTimer()
+	allTrue := true
+	var resp interface{}
+	var ok bool
+	for i := 0; i < b.N; i++ {
+		resp, ok = trie.Get(longest)
+		allTrue = allTrue && resp.(bool) && ok
+	}
+	if !allTrue {
+		b.Fatal("allTrue should always be true")
+	}
+}
+
 func Benchmark_trie128_SmallStr_4(b *testing.B)    { benchmark_trie_SmallStr(b, 4, 128) }
 func Benchmark_trie128_SmallStr_8(b *testing.B)    { benchmark_trie_SmallStr(b, 8, 128) }
 func Benchmark_trie128_SmallStr_16(b *testing.B)   { benchmark_trie_SmallStr(b, 16, 128) }
@@ -68,13 +87,33 @@ func Benchmark_trie128_SmallStr_1M(b *testing.B)   { benchmark_trie_SmallStr(b, 
 
 func benchmark_trie_SmallStr(b *testing.B, keys int, alphaSize int) {
 	runtime.GC()
+	b.ReportAllocs()
 	trie := NewTrie(alphaSize)
+	var longest string
 	for i := 0; i < keys; i++ {
-		trie.Put(fmt.Sprint(i), true)
+		str := fmt.Sprint(i)
+		trie.Put(str, true)
+		if len(longest) < len(str) {
+			longest = str
+		}
 	}
+	// b.Logf("longest=%q", longest)
 	b.ResetTimer()
-	key := fmt.Sprint(keys + 1)
+	allTrue := true
+	var resp interface{}
+	var ok bool
 	for i := 0; i < b.N; i++ {
-		_, _ = trie.Get(key)
+		resp, ok = trie.Get(longest)
+		allTrue = allTrue && resp.(bool) && ok
 	}
+	if !allTrue {
+		b.Fatal("allTrue should always be true")
+	}
+}
+
+func max(a, b int) int {
+	if a > b {
+		return a
+	}
+	return b
 }
